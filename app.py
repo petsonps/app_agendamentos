6609900dc8482479dd15450bdf13ff84d5c6ce4d
PetsonMac <petsonps@hotmail.com>
2024-06-03 21:50:38 -0300

    v1.0.1

--- app.py
+++ app.py
@@ -165,4 +165,5 @@ def validate_phone(phone):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
